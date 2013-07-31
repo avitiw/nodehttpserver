7d171c2d9ce1fa89d374ce4dfdc0cbf70dd649aa
Avinash Tiwari <avitiw@gmail.com>
2013-07-31 12:09:49 +0530

    updated port selected

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var http = require('http'),
  webServer = require('./lib/webserver');
 
 var server = http.createServer();
- 
+ var port = process.env.PORT || 8124;
 var webServer = new webServer(server);
-server.listen(8124);
+server.listen(port);
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://127.0.0.1:'+ port);
 
