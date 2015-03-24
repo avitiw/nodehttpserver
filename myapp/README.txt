40cb83c7c6f40fa95f09e7d547606cbb534fdd6e
avitiw <avitiw@gmail.com>
2015-03-24 10:49:22 +0000

    Kafka integration

---
+++ myapp/README.txt
@@ -0,0 +1,23 @@
+First install NodeJS
+then open command prompt and got to "myapp" directiory
+Run: 
+	npm install
+Afer successfull install : 
+	set DEBUG=myapp & node  .\bin\www
+
+-----------------
+For Kafka 
+
+Unzip kafka. 
+Modify config properties
+ 1. zookeeper.properties change dataDir
+ 2. server.properties change log.dirs
+
+ In one console windows run zookeper script from the kafka root window
+ 	bin\windows\zookeeper-server-start.bat config\zookeeper.properties
+ In another console window run kafka server script
+ 	bin\windows\kafka-server-start.bat config\server.properties
+ TO create topic "travelRequest"
+ 	bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic travelRequest
+ To check if topic is created
+    bin\windows\kafka-topics.bat --list --zookeeper localhost:2181 
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+var kafka = require('kafka-node');
+    
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var reqData = {
@@ -15,8 +18,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/',function(req,res,next){
-	console.log(util.inspect(req.body, false, null));
-	 
+	//console.log(util.inspect(req.body, false, null));
+	payloads = [
+        { topic: 'travelRequest', 
+        	messages : {
+        		start : { Lat : req.body.startLat, Lng : req.body.startLng},
+        		end : { Lat : req.body.endLat, Lng : req.body.endLng}
+        	} 
+        }         
+    ];
+    
+    var HighLevelProducer = kafka.HighLevelProducer,
+    client = new kafka.Client(),
+    producer = new HighLevelProducer(client);
+	 producer.on('ready', function () {
+    producer.send(payloads, function (err, data) {
+	        console.log(util.inspect(data, false, null));
+	    });
+	});
 	res.render('index',{title :"Express",data: req.body})
 });
 module.exports = router;
